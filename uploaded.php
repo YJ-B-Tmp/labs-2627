96cb950633fe1a59ee846a3628a257ae4413b128
Ysabelle Joy C Bengco <ysabellejoy918@gmail.com>
2026-08-20 18:03:05 +0800

    img test png

--- uploaded.php
+++ uploaded.php
@@ -16,6 +16,20 @@ if (move_uploaded_file($temporary_file, $uploaded_text_file)) {
     echo 'Failed to upload file';
 }
 
+// pls add any image file it should work this tiem
+if (isset($_FILES['image_file']) && $_FILES['image_file']['error'] === UPLOAD_ERR_OK) {
+    $uploaded_image_file = $upload_directory . basename($_FILES['image_file']['name']);
+    $temporary_image = $_FILES['image_file']['tmp_name'];
+
+    if (move_uploaded_file($temporary_image, $uploaded_image_file)) {
+        $image_relative_path = $relative_path . basename($_FILES['image_file']['name']);
+        ?>
+        <img src="<?php echo $image_relative_path; ?>" alt="Uploaded image" width="400">
+        <?php
+    } else {
+        echo 'Failed to upload image file';
+    }
+}
 
 echo '<pre>';
 var_dump($_FILES);
